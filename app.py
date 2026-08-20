aa3ced89982f5f64216e038b9c7f62613028b974
root708 <papaaronasene123@gmail.com>
2026-08-20 16:42:59 +0000

    Add house price prediction app with Streamlit
    Implement house price prediction using Streamlit and XGBoost model.

---
+++ app.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import streamlit as st
+import joblib
+
+model = joblib.load('xgb_model.jb')
+st.title("House Price Prediction")
+st.write("Enter the Details Below to predict the House Price")
+
+input['OverallQual', 'GrLivArea', 'ExterQual', 'GarageArea',
+       'BsmtQual', '1stFlrSF', 'KitchenQual', 'FullBath', 'GarageFinish',
+       'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'Fireplaces', 'GarageType',
+       'HeatingQC', 'BsmtFinSF1', 'LotFrontage', 'WoodDeckSF', 'OpenPorchSF',
+       'BsmtExposure', 'LotArea', 'LotShape', 'CentralAir']
+
+input_data = {}
+for features in input:
+    if features == 'CentralAir':
+        input_data[features] = st.selectbox(f"{features}", options=['Yes','No'], index=0)
+    else:
+        input_data[features] = st.number_input(
+            f"{features}",
+            value=0.0,
+            step=1.0 if features in ['OverallQual','FullBath','Fireplaces'] else 0.1
+        )
+
+if st.button("Predict Price"):
+    input_data['CentralAir'] =1 if input_data['CentralAir'] == "Yes" else 0
+
+    input_df = pd.DataFrame([input_data],columns=input)
+
+    predictions = model.predict(input_df)
+    st.success(f"Predicted House Price: ${predictions[0]:,.2f}")
